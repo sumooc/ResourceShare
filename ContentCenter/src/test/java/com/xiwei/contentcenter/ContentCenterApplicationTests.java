f2b112b25ffaad10a41cfa54d8c89848bbb1eb7a
sumooc <pj1993@163.com>
2023-04-23 11:36:54 +0800

    创建项目

---
+++ ContentCenter/src/test/java/com/xiwei/contentcenter/ContentCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiwei.contentcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
