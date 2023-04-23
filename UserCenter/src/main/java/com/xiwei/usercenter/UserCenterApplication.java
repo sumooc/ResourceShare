f887ad6685ca2cdd6a400f1dc611fee76116ce9a
sumooc <pj1993@163.com>
2023-04-23 14:38:44 +0800

    添加Druid数据源

--- UserCenter/src/main/java/com/xiwei/usercenter/UserCenterApplication.java
+++ UserCenter/src/main/java/com/xiwei/usercenter/UserCenterApplication.java
@@ -2,7 +2,9 @@ package com.xiwei.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.xiwei")
 @SpringBootApplication
 public class UserCenterApplication {
 
