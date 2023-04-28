77328ee65b9b9b7272b0086c1329fe1046cad038
sumooc <pj1993@163.com>
2023-04-28 23:31:29 +0800

    添加nacos服务发现

--- ContentCenter/src/main/java/com/xiwei/contentcenter/service/content/ShareService.java
+++ ContentCenter/src/main/java/com/xiwei/contentcenter/service/content/ShareService.java
@@ -6,8 +6,12 @@ import com.xiwei.contentcenter.domain.dto.user.UserDTO;
 import com.xiwei.contentcenter.domain.entity.content.Share;
 import jakarta.annotation.Resource;
 import org.springframework.beans.BeanUtils;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
 public class ShareService {
@@ -15,12 +19,18 @@ public class ShareService {
     private ShareMapper shareMapper;
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     public ShareDTO findById(Integer id) {
         Share share = shareMapper.selectByPrimaryKey(id);
         Integer userId = share.getUserId();
-        UserDTO userDTO = restTemplate.getForObject("http://127.0.0.1:8080/users/{id}", UserDTO.class, userId);
 
+        List<ServiceInstance> instances = discoveryClient.getInstances("UserCenter");
+        String targetUrl = instances.stream().map(instance -> instance.getUri().toString() + "/users/{id}")
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("当前没有实例"));
+        UserDTO userDTO = restTemplate.getForObject(targetUrl, UserDTO.class, userId);
         ShareDTO shareDTO = new ShareDTO();
         BeanUtils.copyProperties(share, shareDTO);
         shareDTO.setWxNickName(userDTO.getWxNickname());
--- UserCenter/src/main/java/com/xiwei/usercenter/UserCenterApplication.java
+++ UserCenter/src/main/java/com/xiwei/usercenter/UserCenterApplication.java
@@ -2,10 +2,12 @@ package com.xiwei.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @MapperScan("com.xiwei")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserCenterApplication {
 
     public static void main(String[] args) {
