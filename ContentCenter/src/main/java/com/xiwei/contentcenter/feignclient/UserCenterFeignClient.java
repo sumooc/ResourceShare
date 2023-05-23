f753e287470856db69727901ae8a573730951669
sumooc <pj1993@163.com>
2023-05-23 23:51:39 +0800

    添加feign

--- ContentCenter/src/main/java/com/xiwei/contentcenter/feignclient/UserCenterFeignClient.java
+++ ContentCenter/src/main/java/com/xiwei/contentcenter/feignclient/UserCenterFeignClient.java
@@ -1,15 +1,15 @@
 package com.xiwei.contentcenter.feignclient;
 
-import com.xiwei.contentcenter.domain.dto.content.ShareDTO;
+import com.xiwei.contentcenter.configuration.UserCenterFeignConfiguration;
 import com.xiwei.contentcenter.domain.dto.user.UserDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "UserCenter")
+@FeignClient(name = "UserCenter", configuration = UserCenterFeignConfiguration.class)
 public interface UserCenterFeignClient {
 
     @GetMapping("/users/{id}")
-    public UserDTO findById(@PathVariable Integer id);
+    UserDTO findById(@PathVariable Integer id);
 
 }
